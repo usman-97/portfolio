954028f022ba257417c28eacbf711912ecd51d66
usman-97 <usmanshabir65@hotmail.com>
2026-03-20 20:24:54 +0000

    Add link to side nav bar

--- src/components/layout/SideNavBar.jsx
+++ src/components/layout/SideNavBar.jsx
@@ -1,8 +1,48 @@
-import React from "react";
+import { ChevronDown, ChevronRight } from "lucide-react";
+import { LuFolderCode } from "react-icons/lu";
+import { FaReact, FaFolderOpen, FaReadme } from "react-icons/fa";
+import { BsCss } from "react-icons/bs";
+import { Link } from "react-router-dom";
 
 const SideNavBar = () => {
   return (
-    <nav className="row-start-2 row-end-4 p-5 bg-midnight">Sidebar Nav</nav>
+    <nav className="row-start-2 row-end-4 bg-midnight text-silver font-mono">
+      <div className="flex items-center p-2 border-b-2 border-b-steel">
+        <h2 className="text-sm uppercase">Explorer</h2>
+      </div>
+      <div className="mt-2 pl-2 pr-4 text-xs">
+        <div className="flex items-end">
+          <ChevronDown />
+          <p className="uppercase font-medium">Portfolio</p>
+        </div>
+        <div className="flex pl-6 pr-4 py-1.5 items-center space-x-1 font-medium">
+          <ChevronDown />
+          <LuFolderCode />
+          <p>src</p>
+        </div>
+        <div className="flex pl-10 pr-4 py-1.5 items-center space-x-1">
+          <FaReact />
+          <p>Home.jsx</p>
+        </div>
+        <div className="flex pl-10 pr-4 py-1.5 items-center space-x-1 font-medium">
+          <ChevronDown />
+          <FaFolderOpen />
+          <p>projects</p>
+        </div>
+        <div className="flex pl-14 pr-4 py-1.5 items-center space-x-1">
+          <FaReadme />
+          <p>App1.md</p>
+        </div>
+        <div className="flex pl-14 pr-4 py-1.5 items-center space-x-1">
+          <FaReadme />
+          <p>App2.md</p>
+        </div>
+        <div className="flex pl-10 pr-4 py-1.5 items-center space-x-1">
+          <BsCss />
+          <p>contact.css</p>
+        </div>
+      </div>
+    </nav>
   );
 };
 
