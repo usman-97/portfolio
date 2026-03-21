b05f88dd46c30c4e405e19483bb1eed900c1b911
usman-97 <usmanshabir65@hotmail.com>
2026-03-21 23:08:04 +0000

    Remove unnecessary imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,3 @@
-import Editor from "./components/layout/Editor";
-import Header from "./components/layout/Header";
-import LeftPanel from "./components/layout/LeftPanel";
-import SideNavBar from "./components/layout/SideNavBar";
 import AppRouter from "./router/AppRouter";
 
 function App() {
