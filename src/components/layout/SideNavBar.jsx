a389a26e032894c0b66f834b8981778cc92bef8d
usman-97 <usmanshabir65@hotmail.com>
2026-03-21 02:44:15 +0000

    Add right side border

--- src/components/layout/SideNavBar.jsx
+++ src/components/layout/SideNavBar.jsx
@@ -3,7 +3,7 @@ import { navItems } from "../../data/navigation";
 
 const SideNavBar = () => {
   return (
-    <nav className="row-start-2 row-end-4 bg-midnight text-silver/50 font-mono">
+    <nav className="row-start-2 row-end-4 bg-midnight text-silver/50 font-mono border-r-2 border-steel">
       <div className="flex items-center p-2 border-b-2 border-b-steel">
         <h2 className="text-sm uppercase">Explorer</h2>
       </div>
