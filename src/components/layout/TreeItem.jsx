561f255397361c4dbf03a3697d6807fc9961672c
usman-97 <usmanshabir65@hotmail.com>
2026-03-21 19:59:35 +0000

    Make nav items to links

--- src/components/layout/TreeItem.jsx
+++ src/components/layout/TreeItem.jsx
@@ -1,5 +1,6 @@
 import { LuChevronDown } from "react-icons/lu";
 import { ICON_MAP } from "../../constants/IconRegistry";
+import { Link } from "react-router-dom";
 
 const TreeItem = ({ item }) => {
   const isFolder = item.type === "folder" || item.type === "root";
@@ -8,31 +9,33 @@ const TreeItem = ({ item }) => {
     (isFolder ? ICON_MAP.defaultFolder : ICON_MAP.defaultFile);
 
   return (
-    <div
-      className="flex items-center py-1.5 space-x-1 hover:bg-steel hover:text-silver cursor-pointer group"
-      style={{
-        paddingLeft: `${item.level * 12 + 8}px`,
-      }}
-    >
-      <span className="w-5 flex items-center justify-center shrink-0">
-        {isFolder && (
-          <LuChevronDown
-            size={20}
-            className="text-silver/30 group-hover:text-silver"
-          />
-        )}
-      </span>
-      {item.type !== "root" && (
-        <span className="flex items-center justify-center shrink-0 mr-1 w-6 text-silver/50 group-hover:text-silver">
-          {icon}
-        </span>
-      )}
-      <p
-        className={`${isFolder ? "font-medium" : "font-normal"} tracking-tight`}
+    <Link to={item.route}>
+      <div
+        className="flex items-center py-1.5 space-x-1 hover:bg-steel hover:text-silver cursor-pointer group"
+        style={{
+          paddingLeft: `${item.level * 12 + 8}px`,
+        }}
       >
-        {item.name}
-      </p>
-    </div>
+        <span className="w-5 flex items-center justify-center shrink-0">
+          {isFolder && (
+            <LuChevronDown
+              size={20}
+              className="text-silver/30 group-hover:text-silver"
+            />
+          )}
+        </span>
+        {item.type !== "root" && (
+          <span className="flex items-center justify-center shrink-0 mr-1 w-6 text-silver/50 group-hover:text-silver">
+            {icon}
+          </span>
+        )}
+        <p
+          className={`${isFolder ? "font-medium" : "font-normal"} tracking-tight`}
+        >
+          {item.name}
+        </p>
+      </div>
+    </Link>
   );
 };
 
