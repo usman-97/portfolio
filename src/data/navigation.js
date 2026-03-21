68df8e2909176d0e466ffd1301a5b6a782fb9b9c
usman-97 <usmanshabir65@hotmail.com>
2026-03-21 23:05:56 +0000

    Add routes for all pages

--- src/data/navigation.js
+++ src/data/navigation.js
@@ -3,7 +3,7 @@ export const navItems = [
   { id: 2, name: 'src', type: 'folder', level: 1, icon: 'src' },
   { id: 3, name: 'Home.jsx', type: 'file', level: 2, icon: 'react', route: '/' },
   { id: 4, name: 'projects', type: 'folder', level: 2, icon: 'projects' },
-  { id: 5, name: 'App1.md', type: 'file', level: 3, icon: 'readme' },
-  { id: 6, name: 'App2.md', type: 'file', level: 3, icon: 'readme' },
+  { id: 5, name: 'App1.md', type: 'file', level: 3, icon: 'readme', route: '/projects/P1000' },
+  { id: 6, name: 'App2.md', type: 'file', level: 3, icon: 'readme', route: '/projects/P1001' },
   { id: 7, name: 'contact.css', type: 'file', level: 2, icon: 'css', route: '/contact' }
 ];
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -7,6 +7,8 @@ import {
 } from "react-router-dom";
 import BaseLayout from "../layouts/BaseLayout";
 import HomePage from "../pages/HomePage";
+import ProjectsPage from "../pages/ProjectsPage";
+import ContactPage from "../pages/ContactPage";
 
 const AppRouter = () => {
   const router = createBrowserRouter(
@@ -14,6 +16,8 @@ const AppRouter = () => {
       <>
         <Route element={<BaseLayout />}>
           <Route path="/" element={<HomePage />} />
+          <Route path="/projects/:projectId" element={<ProjectsPage />} />
+          <Route path="/contact" element={<ContactPage />} />
         </Route>
       </>,
     ),
