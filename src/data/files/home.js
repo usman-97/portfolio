bf98802b49041b3d953aa41037ab26b166b933e9
usman-97 <usmanshabir65@hotmail.com>
2026-03-22 03:56:02 +0000

    Correct key name

--- src/data/files/home.js
+++ src/data/files/home.js
@@ -2,7 +2,7 @@ export const homeContent = `const dev = {
   name: "ANIMATE_NAME",
   role: "Software Engineer",
   status: "Open for Opportunities",
-  Location: "Manchester, UK",
+  location: "Manchester, UK",
 }
 
 // Hello! Click a project in the sidebar to explore my work.`;
