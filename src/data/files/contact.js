17489e0bcd54f7d780990ade364f707f0f08fd99
usman-97 <usmanshabir65@hotmail.com>
2026-03-24 22:56:20 +0000

    Add contact page data

---
+++ src/data/files/contact.js
@@ -0,0 +1,14 @@
+export const contactContent = {
+    content : `/* Status: Open for collaborations */
+.engineer-contact {
+    email: "usmanshabir@hotmail.com",
+    linkedIn: "usman-shabir-kousar",
+    gitHub: "usman-97"
+}`,
+    contactLinks: {
+    "linkedIn": "https://www.linkedin.com/in/usman-shabir-kousar/",
+    "gitHub": "https://github.com/usman-97",
+}
+};
+
+export const terminalContactContent = `!`
