ab8c199bfe90442b5d86ee53238d2b06c01f3d6b
usman-97 <usmanshabir65@hotmail.com>
2026-03-24 22:56:09 +0000

    Add contact page

--- src/data/fileContents.js
+++ src/data/fileContents.js
@@ -1,3 +1,4 @@
+import { contactContent, terminalContactContent } from "./files/contact";
 import { homeContent, terminalHomeContent } from "./files/home";
 import { terminalLogs } from "./files/terminal";
 
@@ -7,6 +8,12 @@ export const fileContents = {
         language: 'javascript',
         content: homeContent,
         terminalContent: terminalHomeContent
+    },
+    'Contact.css': {
+        name: 'Contact.jsx',
+        language: 'css',
+        content: contactContent,
+        terminalContent: terminalContactContent
     },
     'terminalLogs': terminalLogs
 }
--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,7 +1,28 @@
-import React from "react";
+import {
+  EDITOR_CONTAINER_STYLE,
+  LINE_NUMBER_STYLE,
+} from "../constants/editorStyles";
+import CodeSyntaxHighlighter from "../themes/CodeSyntaxHighlighter";
+import { fileContents } from "../data/fileContents";
+import { emeraldSyntaxTheme } from "../themes/themes";
+import { useMobile } from "../hooks/useMobile";
 
 const ContactPage = () => {
-  return <div>ContactPage</div>;
+  const isMobile = useMobile();
+
+  return (
+    <CodeSyntaxHighlighter
+      language={fileContents["Contact.css"].language}
+      codeString={fileContents["Contact.css"].content.content}
+      style={emeraldSyntaxTheme}
+      customStyle={{
+        ...EDITOR_CONTAINER_STYLE,
+        fontSize: isMobile ? "12px" : "1.5rem",
+      }}
+      showLineNumbers={true}
+      lineNumberStyle={LINE_NUMBER_STYLE}
+    />
+  );
 };
 
 export default ContactPage;
