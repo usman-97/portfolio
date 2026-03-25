f75f7492e70525e4eaf81e0fc2f01076c99b098e
usman-97 <usmanshabir65@hotmail.com>
2026-03-25 20:47:59 +0000

    Refactor Terminal Process mode

--- src/components/layout/Terminal.jsx
+++ src/components/layout/Terminal.jsx
@@ -5,6 +5,21 @@ import { getBreadcrumbStack } from "../../utility/Location";
 import { navItems } from "../../data/navigation";
 import TerminalProcessMode from "./terminal/TerminalProcessMode";
 import { terminalData } from "../../constants/TerminalData";
+import { motion, AnimatePresence } from "motion/react";
+
+const container = {
+  hidden: { opacity: 0 },
+  visible: {
+    opacity: 1,
+    transition: { staggerChildren: 0.5, delayChildren: 0.2 },
+  },
+  exit: { opacity: 0, transition: { duration: 0.1 } },
+};
+
+const item = {
+  hidden: { opacity: 0, x: -10 },
+  visible: { opacity: 1, x: 0, transition: { duration: 0.3 } },
+};
 
 const Terminal = ({ activeFile }) => {
   const content = fileContents[activeFile];
@@ -34,22 +49,33 @@ const Terminal = ({ activeFile }) => {
           <IoMdClose />
         </div>
       </div>
-      {content?.terminalContent &&
-        (data.mode === "PROCESS" ? (
-          <TerminalProcessMode
-            key={location.pathname}
-            currentPage={activeFile}
-            headerLogs={content?.terminalContent?.header}
-            footerLogs={content?.terminalContent?.footer}
-            terminalContent={data.body}
-            fullPath={fullPath}
-            commandTag={commandTag}
-            command={data.command}
-            comandTagPath={comandTagPath}
-          />
-        ) : (
-          <div></div>
-        ))}
+      <AnimatePresence mode="wait">
+        <motion.div
+          key={location.pathname}
+          className="flex-col flex-1 space-y-1 p-3 text-xs"
+          variants={container}
+          initial="hidden"
+          animate="visible"
+        >
+          {content?.terminalContent &&
+            (data.mode === "PROCESS" ? (
+              <TerminalProcessMode
+                key={location.pathname}
+                currentPage={activeFile}
+                headerLogs={content?.terminalContent?.header}
+                footerLogs={content?.terminalContent?.footer}
+                terminalContent={data.body}
+                fullPath={fullPath}
+                commandTag={commandTag}
+                command={data.command}
+                comandTagPath={comandTagPath}
+                animateItem={item}
+              />
+            ) : (
+              <span></span>
+            ))}
+        </motion.div>
+      </AnimatePresence>
     </section>
   );
 };
--- src/components/layout/terminal/TerminalProcessMode.jsx
+++ src/components/layout/terminal/TerminalProcessMode.jsx
@@ -1,24 +1,8 @@
-import { motion, AnimatePresence } from "motion/react";
-import React from "react";
+import { motion } from "motion/react";
 import TerminalCommand from "./TerminalCommand";
 import TerminalProcess from "./TerminalProcess";
 
-const container = {
-  hidden: { opacity: 0 },
-  visible: {
-    opacity: 1,
-    transition: { staggerChildren: 0.5, delayChildren: 0.2 },
-  },
-  exit: { opacity: 0, transition: { duration: 0.1 } },
-};
-
-const item = {
-  hidden: { opacity: 0, x: -10 },
-  visible: { opacity: 1, x: 0, transition: { duration: 0.3 } },
-};
-
 const TerminalProcessMode = ({
-  key,
   headerLogs,
   fullPath,
   terminalContent,
@@ -26,38 +10,34 @@ const TerminalProcessMode = ({
   commandTag,
   command,
   comandTagPath,
+  animateItem,
 }) => {
   return (
-    <AnimatePresence mode="wait">
+    <>
+      <TerminalCommand
+        tag={commandTag}
+        command={command}
+        tagPath={comandTagPath}
+      />
+      <TerminalProcess
+        logs={headerLogs}
+        fullPath={fullPath}
+        animateItem={animateItem}
+      />
       <motion.div
-        key={key}
-        className="flex-col flex-1 space-y-1 p-3 text-xs"
-        variants={container}
-        initial="hidden"
-        animate="visible"
+        className="flex flex-col space-y-2 pb-2"
+        variants={animateItem}
       >
-        <TerminalCommand
-          tag={commandTag}
-          command={command}
-          tagPath={comandTagPath}
-        />
-        <TerminalProcess
-          logs={headerLogs}
-          fullPath={fullPath}
-          animateItem={item}
-        />
-        <motion.div className="flex flex-col space-y-2 pb-2" variants={item}>
-          {terminalContent}
-        </motion.div>
-        <TerminalProcess logs={footerLogs} animateItem={item} />
-        <TerminalCommand
-          tag={commandTag}
-          showCursor={true}
-          animateItem={item}
-          tagPath={comandTagPath}
-        />
+        {terminalContent}
       </motion.div>
-    </AnimatePresence>
+      <TerminalProcess logs={footerLogs} animateItem={animateItem} />
+      <TerminalCommand
+        tag={commandTag}
+        showCursor={true}
+        animateItem={animateItem}
+        tagPath={comandTagPath}
+      />
+    </>
   );
 };
 
