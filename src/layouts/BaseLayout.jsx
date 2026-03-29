49e05a647706f98c3e568f8813cbf780e49e0a3c
usman-97 <usmanshabir65@hotmail.com>
2026-03-29 03:00:19 +0100

    Remove input which are not required

--- src/layouts/BaseLayout.jsx
+++ src/layouts/BaseLayout.jsx
@@ -5,8 +5,6 @@ import LeftPanel from "../components/layout/LeftPanel";
 import SideNavBar from "../components/layout/SideNavBar";
 import Terminal from "../components/layout/Terminal";
 import { useState } from "react";
-import { FaCodeBranch } from "react-icons/fa";
-import { IoCloseCircle } from "react-icons/io5";
 import Footer from "../components/layout/Footer";
 
 const BaseLayout = () => {
