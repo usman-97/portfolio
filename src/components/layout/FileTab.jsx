0dd724c5083794132927b1860d824a7ffd94c871
usman-97 <usmanshabir65@hotmail.com>
2026-04-01 19:27:33 +0100

    Dynamically change file name

--- src/components/layout/FileTab.jsx
+++ src/components/layout/FileTab.jsx
@@ -1,10 +1,16 @@
 import { IoMdClose } from "react-icons/io";
+import { useLocation } from "react-router-dom";
+import { navItems } from "../../data/navigation";
 
 const FileTab = () => {
+  const location = useLocation();
+  const currentPageItem = navItems.find(
+    (item) => item.route === location.pathname,
+  );
   return (
     <div className="flex justify-start border-b border-steel">
       <div className="flex items-center justify-between py-2 pl-3 pr-2 bg-midnight text-sm">
-        <span className=" pr-2">Home.jsx</span>
+        <span className="pr-2 text-silver/60">{currentPageItem?.name}</span>
         <IoMdClose size={16} />
       </div>
     </div>
