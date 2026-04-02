90d32dc3eb6619ae33e1b7aea237013aa7a30002
usman-97 <usmanshabir65@hotmail.com>
2026-04-02 23:38:45 +0100

    Remove item

--- src/data/navigation.js
+++ src/data/navigation.js
@@ -4,6 +4,5 @@ export const navItems = [
   { id: 3, name: 'Home.jsx', type: 'file', level: 2, icon: 'react', route: '/', parentId: 2 },
   { id: 4, name: 'projects', type: 'folder', level: 2, icon: 'projects', parentId: 'src' },
   { id: 5, name: 'simple-weather-app.md', type: 'file', level: 3, icon: 'readme', route: '/projects/P1000', parentId: 4 },
-  { id: 6, name: 'App2.md', type: 'file', level: 3, icon: 'readme', route: '/projects/P1001', parentId: 4 },
   { id: 7, name: 'contact.css', type: 'file', level: 2, icon: 'css', route: '/contact', parentId: 2 }
 ];
