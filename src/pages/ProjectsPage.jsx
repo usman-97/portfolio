d1f83f5da710f583cea4e18bd4b275fe8f580944
usman-97 <usmanshabir65@hotmail.com>
2026-04-03 02:48:38 +0100

    Adjust project page for different screen sizes

--- src/pages/ProjectsPage.jsx
+++ src/pages/ProjectsPage.jsx
@@ -17,8 +17,8 @@ const ProjectsPage = () => {
       <h2 className="pb-1 text-emerald text-lg font-extrabold border-b border-emerald/60 md:text-3xl">
         {content.name}
       </h2>
-      <div className="flex md:space-x-8">
-        <div className="hidden flex-col space-y-6 w-full lg:flex">
+      <div className="flex md:space-x-8 lg:flex-col 2xl:flex-row">
+        <div className="hidden lg:flex lg:flex-col lg:self-center lg:space-y-6 lg:w-md lg:my-5 2xl:w-full 2xl:self-auto 2xl:my-0">
           <motion.img
             animate={{ y: [0, -5, 0] }}
             transition={{ duration: 2, repeat: Infinity, ease: "easeOut" }}
