f28e064724e1de465a7a2ed1607b23015687a785
usman-97 <usmanshabir65@hotmail.com>
2026-05-16 01:31:16 +0100

    Add env for sanity config

--- src/lib/sanity.js
+++ src/lib/sanity.js
@@ -1,8 +1,9 @@
 import { createClient } from '@sanity/client';
+import { SANITY_API_VERSION, SANITY_DATASET, SANITY_PROJECT_ID, SANITY_USECDN } from '../config/config';
 
 export const client = createClient({
-    projectId: 'sa1bj9p5',
-    dataset: 'production',
-    useCdn: true,
-    apiVersion: '2026-04-18'
+    projectId: SANITY_PROJECT_ID,
+    dataset: SANITY_DATASET,
+    useCdn: SANITY_USECDN,
+    apiVersion: SANITY_API_VERSION
 });
